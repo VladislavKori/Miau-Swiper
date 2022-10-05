c6fe75e7e32f00ee8a1fb9dd61799d1b9cdc984f
vladislav kori <vladislav.kori.work@gmail.com>
2022-10-05 18:29:19 +0500

    [FIX] scroll fix

--- src/App.jsx
+++ src/App.jsx
@@ -11,9 +11,7 @@ function App() {
          Smt. sidebar content
       </aside>
 
-      <div className="container">
-        <Slider />
-      </div>
+      <Slider />
 
     </div>
   )
--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react'
 
 import './slider.css'
 
@@ -6,17 +6,30 @@ import Slide from './slide/slide'
 
 function Slider() {
 
+  const slider = useRef(null)
+
+  useEffect(() => {
+    const innerSlider = slider.current;
+    const sliderListener = innerSlider.addEventListener('click', e => {
+      console.log(e)
+    })
+
+    return () => removeEventListener('click', sliderListener)
+  }, [])
+
   const elemArrow = new Array(20).fill('simple text');
 
   return (
-    <div className="slider">
-      <ul className="slider__list">
-        {elemArrow.map((item, index) => (
-          <React.Fragment key={index}>
-            <Slide text={item} />
-          </React.Fragment>
-        ))}
-      </ul> 
+    <div className="container" ref={slider}>
+      <div className="slider" >
+        <ul className="slider__list">
+          {elemArrow.map((item, index) => (
+            <React.Fragment key={index}>
+              <Slide text={item} />
+            </React.Fragment>
+          ))}
+        </ul>
+      </div>
     </div>
   )
 }
